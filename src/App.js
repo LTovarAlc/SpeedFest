90f11d9d477689d508b77ad8228b417735ddf86c
Luis Tovar <ltovaralc@gmail.com>
2024-03-18 11:12:41 -0600

    agregando nombre de la pagina e icono de la pagina

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./reset.css"
 import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Home/>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+    </Router>
   )
 }
 
