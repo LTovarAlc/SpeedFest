f0a29aadb464ad0bf8b91457b7dcbd21212e51f8
Luis Tovar <ltovaralc@gmail.com>
2024-03-19 13:57:54 -0600

    banner 1 y 2 completados

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,38 +1,61 @@
 import React from "react";
-import "animate.css"
+import "animate.css";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./Banner.css";
 
 const Banner = () => {
-    const settings = {
-        dots: false,
-        infinite: true,
-        speed: 1000, // Velocidad de transición en milisegundos
-        slidesToShow: 1, // Cantidad de imágenes a mostrar a la vez
-        slidesToScroll: 1, // Cantidad de imágenes a desplazar en cada cambio
-        autoplay: true, // Reproducción automática
-        autoplaySpeed: 10000, // Intervalo de cambio en milisegundos (10 segundos)
-        arrows: false,
-    };
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 1000, // Velocidad de transición en milisegundos
+    slidesToShow: 1, // Cantidad de imágenes a mostrar a la vez
+    slidesToScroll: 1, // Cantidad de imágenes a desplazar en cada cambio
+    autoplay: true, // Reproducción automática
+    autoplaySpeed: 7000, // Intervalo de cambio en milisegundos (10 segundos)
+    arrows: false,
+  };
 
-    return (
-        <section className="banner">
-            <Slider {...settings}>
-                <div>
-                    <img src="../../../img/logoRace.png" className="speedFest__logo animate__animated animate__fadeInUp animate__delay-2s" />
-                    <img src="../../../img/banner1Speed.jpg" alt="Banner1" className="banner__img" />
-                </div>
-                <div>
-                    <img src="../../../img/banner2.jpg" alt="Banner2" className="banner__img" />
-                </div>
-                <div>
-                    <img src="../../../img/banner3.jpg" alt="Banner3" className="banner__img" />
-                </div>
-            </Slider>
-        </section>
-    );
+  return (
+    <section className="banner">
+      <Slider {...settings}>
+        <div>
+          <img
+            src="../../../img/logoRace.png"
+            className="speedFest__logo animate__animated animate__fadeInUp animate__delay-2s"
+          />
+          <h1 className="slogan animate__animated animate__fadeInUp animate__delay-2s">
+            El festival de carros deportivos mas inmenso del continente
+          </h1>
+          <img
+            src="../../../img/banner1Speed.jpg"
+            alt="Banner1"
+            className="banner__img"
+          />
+        </div>
+        <div>
+          <h1 className="presentation">Presentado por:</h1>
+          <img src="../../../img/LogoPirelli.png" className="pirelli" />
+          <h1 className="slogan animate__animated animate__fadeInUp animate__delay-2s">
+            Con una gran exposición de autos deportivos y show de driffting
+          </h1>
+          <img
+            src="../../../img/banner2.jpg"
+            alt="Banner2"
+            className="banner__img"
+          />
+        </div>
+        <div>
+          <img
+            src="../../../img/banner3.jpg"
+            alt="Banner3"
+            className="banner__img"
+          />
+        </div>
+      </Slider>
+    </section>
+  );
 };
 
 export default Banner;
